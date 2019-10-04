1c5db47b59d5e86471012422d8528d735b4daa0a
haquesh <shadid.haque@cibc.com>
2019-10-04 16:36:04 -0400

    added colors

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from '../components/Nav'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,16 +14,44 @@ const useStyles = makeStyles(theme => ({
     padding: '20px',
     display: 'flex'
   },
+  paper: {
+    marginBottom: '10px',
+    backgroundColor: '#f06292'
+  },
   img: {
     height: '50px',
     marginTop: '20px',
     borderRadius: '50%',
     paddingRight: '10px'
+  },
+  button: {
+    margin: theme.spacing(1),
+    textTransform: 'none'
+  },
+  p: {
+    backgroundColor: '#40c4ff',
+    padding: '5px',
+    borderRadius: '5px'
   }
 }));
 
 const Home = () => {
   const classes = useStyles()
+
+  const [posts, setPosts] = useState([
+    {
+      id: '1',
+      img: '/static/assets/1.jpg',
+      title: 'Article number 1',
+      body: 'Short Summary of the existing article'
+    },
+    {
+      id: '2',
+      img: '/static/assets/1.jpg',
+      title: 'Article number 2',
+      body: 'Short Summary of the existing article'
+    }
+  ]);
 
   return(
     <div>
@@ -32,17 +61,29 @@ const Home = () => {
       <div>
         <Grid container spacing={3}>
         <Grid item sm={4} >
-          <Paper className={classes.root}>
-            <img src='/static/assets/1.jpg' className={classes.img}/>
-            <div>
-              <Typography variant="h6" component="h3">
-                This is a sheet of paper.
-              </Typography>
-              <Typography component="p">
-                Paper can be used to build surface or other elements for your application.
-              </Typography>
-            </div>
-          </Paper>
+          <Typography variant="h5" component="h3" className={classes.root}>
+            Recent Articles
+          </Typography>
+          {
+            posts.map(post => {
+              return(
+                <Paper className={`${classes.root} ${classes.paper}`} key={post.id}>
+                  <img src={post.img} className={classes.img}/>
+                  <div>
+                    <Typography variant="h6" component="h3">
+                      {post.title}
+                    </Typography>
+                    <Typography component="p" className={classes.p}>
+                      {post.body}
+                    </Typography>
+                    <Button variant="contained" color="primary" className={classes.button}>
+                      Read More
+                    </Button>
+                  </div>
+                </Paper>
+              )
+            })
+          }
         </Grid>
         </Grid>
       </div>
